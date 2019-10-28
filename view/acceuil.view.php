7decb8c8ee24deb14686eacba7bd64187c9dc47e
elwalid KADURA <47081320+wawkadura@users.noreply.github.com>
2019-10-28 17:39:20 +0100

    w

--- view/acceuil.view.php
+++ view/acceuil.view.php
@@ -2,7 +2,7 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="design/style.css">
+    <link rel="stylesheet" href="design/page.style.css">
     <link rel="icon" type="image/png" href="../model/data/icons/logo1.png" />
     <title>GameStock.com</title>
   </head>
