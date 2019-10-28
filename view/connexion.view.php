5e90b429f27e4172f64973a11e271eb036e71d66
KADURA El Walid <kadurae@hostname.iut2.upmf-grenoble.fr>
2019-10-28 17:32:59 +0100

    f

--- view/connexion.view.php
+++ view/connexion.view.php
@@ -6,48 +6,5 @@
   </head>
   <body>
     <h1>CONNEXION</h1>
-    <div class="fancybox-skin" style="padding: 15px; width: auto; height: auto;" wfd-id="2"><div class="fancybox-outer" wfd-id="3"><div class="fancybox-inner" style="overflow: auto; width: 710px; height: auto;" wfd-id="4"><div id="ig-loginbox-register-ajax-container" style="display: block;" wfd-id="5">
-<h2 class="ig-register-ajax-header">Créez votre compte IG pour continuer</h2>
-<div class="ig-wrapper2" id="ig-productbuy-login" wfd-id="6">
-<div class="ig-productbuy-login-leftpanel" wfd-id="13">
-<h3>Se connecter avec</h3>
-<div class="ig-loginbox-wrapper" wfd-id="14">
-<a href="/fr/creer-un-compte/" class="signin-btn signin-btn-fb"></a>
-<a href="/fr/creer-un-compte/" class="signin-btn signin-btn-google"></a>
-<center>
-<div class="or" wfd-id="15">ou</div>
-<a href="/fr/creer-un-compte/" id="ig-register-manual">Créer mon compte manuellement</a>
-</center>
-</div>
-</div>
-<div class="ig-productbuy-login-rightpanel" wfd-id="9">
-<h3>J'ai déjà un compte IG</h3>
-<div class="ig-loginbox-wrapper" wfd-id="10">
-<form class="ig-login-form" action="https://www.instant-gaming.com/fr/" method="post" wfd-id="12">
-<input class="ig-login-email ig-login-input" type="email" name="email" value="Email" required="" wfd-id="610">
-<input class="ig-login-pass ig-login-input" type="password" name="password" value="*********" required="" wfd-id="609">
-<input type="hidden" name="usid" value="d4850a79728629608d0f83036235df06" wfd-id="608">
-<input type="hidden" name="process" value="login" wfd-id="607">
-<input type="hidden" name="ajax" value="true" wfd-id="606">
-<input class="ig-login-submit ig-login-input-ok validated" type="submit" value="OK" wfd-id="605">
-<kpm-button class="kpm_input-field-button kpm_gray-key-icon kpm_sign-in-button" style="z-index: 8032 !important;"><svg width="12" height="24" class="kpm_reset" viewBox="196 276 8 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="top: -43px !important; left: -27.375px !important; width: 12px !important; height: 24px !important;">
-        <path d="M202.012,283.439v6.41h0l-1.971,2.278-2.111-2.139,1.255-1.482-1.193-.889V286.51l1.006-1.046-1.006-1.076v-0.949A4,4,0,1,1,202.012,283.439ZM200,278a1,1,0,1,0,1,1A1,1,0,0,0,200,278Zm1.006,7.7a0.5,0.5,0,1,0-1.006,0v3.533a0.5,0.5,0,1,0,1.006,0V285.7Z">
-        </path>
-    </svg></kpm-button><kpm-button class="kpm_input-field-button kpm_gray-key-icon kpm_sign-in-button" style="z-index: 8034 !important;"><svg width="12" height="24" class="kpm_reset" viewBox="196 276 8 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="top: 10px !important; left: -90.375px !important; width: 12px !important; height: 24px !important;">
-        <path d="M202.012,283.439v6.41h0l-1.971,2.278-2.111-2.139,1.255-1.482-1.193-.889V286.51l1.006-1.046-1.006-1.076v-0.949A4,4,0,1,1,202.012,283.439ZM200,278a1,1,0,1,0,1,1A1,1,0,0,0,200,278Zm1.006,7.7a0.5,0.5,0,1,0-1.006,0v3.533a0.5,0.5,0,1,0,1.006,0V285.7Z">
-        </path>
-    </svg></kpm-button></form>
-<div class="ig-login-links" wfd-id="11"><a href="https://www.instant-gaming.com/fr/mot-de-passe-oublie/">Mot de passe oublié ?</a></div>
-</div>
-</div>
-<div class="clear" wfd-id="8"></div>
-<p id="ig-loginbox-giveaways" style="display: none">La création de votre compte nous permettra de vous livrer votre jeu immédiatement dès la fin du giveaway !</p>
-<p>Vos données ne seront jamais utilisées ou revendues à des fins commerciales.
-<span id="ig-loginbox-contactdetails" style="display: none" wfd-id="7">
-<br><br><small><small>Instant Gaming Ltd, St 2611, Langham Place Off Tw, 8 Argyle Street Mongkok, HK - RN 1808100<br>
-Payop Ltd, 71-75 Shelton Street, Covent Garden, WC2H9JQ London, UK - RN 10390479<small></small>
-</small></small></span><small><small>
-</small></small></p><small><small>
-</small></small></div><small><small> </small></small></div></div></div><a title="Close" class="fancybox-item fancybox-close" href="javascript:;"></a></div>
-  </body>
+    </body>
 </html>
