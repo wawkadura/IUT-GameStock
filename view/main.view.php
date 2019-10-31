69b60520808b5d85ce4cc1a0983ac4bad5c29420
WATRIN Cesar <watrinc@hostname.iut2.upmf-grenoble.fr>
2019-11-01 00:36:58 +0100

    update

--- view/main.view.php
+++ view/main.view.php
@@ -20,38 +20,50 @@
           </form>
       </div>
 
-      <div class="onglets">
+      <nav class="onglets">
         <ul>
         <!--  <li><a href="../controler/main.ctrl.php?onglet=EpicGames">Epic Games</a></li>-->
         <!--  <li><a href="../controler/main.ctrl.php?onglet=Battle.net">Battle.net</a></li>-->
         <!--  <li><a href="../controler/main.ctrl.php?onglet=GOG.com">GOG.com</a></li> -->
-          <li><a href="../controler/main.ctrl.php?onglet=Nintendo">Nintendo</a></li>
-          <li><a href="../controler/main.ctrl.php?onglet=Origin">Origin</a></li>
-          <li><a href="../controler/main.ctrl.php?onglet=Playstation">Playstation</a></li>
-          <li><a href="../controler/main.ctrl.php?onglet=Steam">Steam</a></li>
-          <li><a href="../controler/main.ctrl.php?onglet=Uplay">Uplay</a></li>
-          <li><a href="../controler/main.ctrl.php?onglet=Xbox">Xbox</a></li>
-          <li class="dropdown"><a href="#">Mon compte</a>
-              <ul class="dropdown-child">
-                <li><a href="../controler/connexion.ctrl.php">Se connecter</a></li>
-                <li><a href="../controler/inscription.ctrl.php">S'inscrire</a></li>
-              </ul>
-          </li>
+          <li><a class="plateforme" href="../controler/main.ctrl.php?onglet=Nintendo">Nintendo</a></li>
+          <li><a class="plateforme" href="../controler/main.ctrl.php?onglet=Origin">Origin</a></li>
+          <li><a class="plateforme" href="../controler/main.ctrl.php?onglet=Playstation">Playstation</a></li>
+          <li><a class="plateforme" href="../controler/main.ctrl.php?onglet=Steam">Steam</a></li>
+          <li><a class="plateforme" href="../controler/main.ctrl.php?onglet=Uplay">Uplay</a></li>
+          <li><a class="plateforme" href="../controler/main.ctrl.php?onglet=Xbox">Xbox</a></li>
+          <!-- <li class="dropdown"><a href="#">Mon compte</a>
+              <ul class="dropdown-child"> -->
+          <li><a class="compte" href="../controler/connexion.ctrl.php">Se connecter</a></li>
+          <li><a class="compte" href="../controler/inscription.ctrl.php">S'inscrire</a></li>
+              <!-- </ul>
+          </li> -->
       </ul>
-     </div>
+    </nav>
   </div>
 
   <div id="milieupage">
+    <ul id="triage">
+	     <li><a href="#">Trier par :</a>
+		       <ul>
+			          <li><a href="#">Date</a></li>
+			          <li><a href="#">Prix Croissant</a></li>
+			          <li><a href="#">Prix Décroissant</a></li>
+			          <li><a href="#">Note des joueurs</a></li>
+		      </ul>
+	    </li>
+    </ul>
+
     <?php
       for ($i=0; $i <count($list) ; $i++) {
         $jeu=$list[$i];
         $img=$images->getCover($jeu[0]);
         $titre=$jeu[1];
-        echo "<p> <a href=\"../controler/jeu.ctrl.php?id=$jeu[0]\"><img src=$chemin/$img[1] alt=\"\" width=\"140px\" height=\"180px\"> $titre </a></p> ";
+        echo "<a href=\"../controler/jeu.ctrl.php?id=$jeu[0]\"><img src=$chemin/$img[1] alt=\"\" width=\"240px\" height=\"280px\"> <p>$titre</p> </a>";
       }
      ?>
   </div>
   <div id="baspage">
+
   </div>
 
 
