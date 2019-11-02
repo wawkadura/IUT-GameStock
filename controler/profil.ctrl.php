590c149b308f87fbfe4d672c5d62d824a45eff95
WATRIN Cesar <watrinc@hostname.iut2.upmf-grenoble.fr>
2019-11-02 12:22:33 +0100

    profil

--- controler/profil.ctrl.php
+++ controler/profil.ctrl.php
@@ -4,6 +4,7 @@ if (isset($_GET['id'])) {
 }else {
   $id=1;
 }
+require_once('../model/JeuDAO.class.php');
 require_once('../model/AdherentDAO.class.php');
 require_once('../model/CommentaireDAO.class.php');
 
@@ -11,6 +12,8 @@ $config = parse_ini_file('../config/config.ini');
 
 $adherents = new AdherentDAO($config['database']);
 $commentaires = new CommentaireDAO($config['database']);
+
+$jeux = new JeuDAO($config['database']);
 
 $profil=$adherents->getAdherent($id);
 $listcom=$commentaires->getCommentairesAdherent($profil['pseudo']);
--- view/profil.view.php
+++ view/profil.view.php
@@ -29,7 +29,41 @@
   </div>
 
   <div id="milieupage">
+    <fieldset class="jeu">
+      <?php
+        echo "<h1>Profil de $profil[1]</h1>";
+        echo "<a href=\"../controler/profil.ctrl.php?id=$profil[0]\"><img src=\"../model/data/icons/user.png\" alt=\"\" width=\"440px\" height=\"440px\"> <p id=\"prix\">$profil[1] $profil[2]</p> </a>";
+        echo "<h2> Prénom : $profil[1]</h2>";
+        echo "<h2> Nom : $profil[2]</h2>";
+        echo "<h2> Email : $profil[4]</h2>";
+        echo "<h2> Pseudo : $profil[3]</h2>";
+        echo "<h2> Mot de passe : $profil[5]</h2>";
 
+
+
+        if ($listcom!=null) {
+          foreach ($listcom as $value) {
+            $pseudo = $value[0];
+            $dateAjout = $value[1];
+            $message = $value[2];
+            $numJeu = $value[3];
+            $jeu=$jeux->getJeux($numJeu);
+            $titre = $jeu[1];
+
+            echo "<fieldset class=\"commentaire\">";
+            echo "<legend class=\"commentaireTitre\">Votre commentaire pour $titre: </legend>";
+            echo "<h5>$dateAjout<h5>";
+            echo "<h5>$message<h5>";
+            echo "</fieldset>";
+            }
+        } else {
+          echo "<fieldset class=\"commentaire\">";
+          echo "<legend class=\"commentaireTitre\">L'équipe GameStock</legend>";
+          echo "<h4>Vous n'avez actuellement posté aucun commentaire sur un jeu ! </h4>";
+          echo "</fieldset>";
+        }
+     ?>
+    </fieldset>
   </div>
 
   <div id="baspage">
